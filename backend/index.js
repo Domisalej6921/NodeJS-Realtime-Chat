e994e238c2f6bcb0fc0096ed1ff87b1a09e41e79
C23069362 <georged8@cardiff.ac.uk>
2024-10-25 14:41:34 -0700

    Developed backend to now connect to establish a connection to MongoDB. Added .env file and added additional dependencies that I will need.

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
 //--------------------------------------------------------------
 // Required code to run Node.js
 const express = require("express"); // Runs HTTP Server
-const cors = require("cors"); 
+const cors = require("cors");
+const mongoose = require("mongoose"); // Helps connect MongoDB 
 
 const app = express();
+require("dotenv").config(); // Allows us to use .env file
+
+//Adds additional functionality to application
 app.use(express.json());
 app.use(cors({ origin: true })); // Gives us ability to call server from any other origin
 
@@ -12,5 +16,18 @@ app.post("/authenticate", async (req, res) => { // Create a POST endpoint for au
   return res.json({ username: username, secret: "sha256..."}); // Returns fake username with password
 });
 
-app.listen(3001); // Run app on port 3001
-//-------------------------------------------------------------
+//Configure port (DEFAULT: 5000)
+const port = process.env.PORT || 5000;
+
+//Retreive connection URI from .env file
+const uri = process.env.ATLAS_URI;
+
+app.listen(port, (req, res) =>{ //Run server on port
+  console.log(`Server is running on ${port}`);
+});
+
+//Create connection to MongoDB
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log("MongoDB Connection Established......")).catch((error) => console.log("MongoDB Connection Failed: ", error.message));
