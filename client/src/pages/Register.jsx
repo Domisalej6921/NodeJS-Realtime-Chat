1685a62a7085c7b69a1d2a72ffc6e4029e940979
C23069362 <georged8@cardiff.ac.uk>
2024-10-26 16:08:01 -0700

    Finished the linking of front-end and back-end on the register user page. User data is retreived from input fields and is sent + stored on the database.

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -4,11 +4,11 @@ import { AuthContext } from "../context/AuthContext";
 
 const Register = () => {
 
-  const { registerInfo, updateRegisterInfo } = useContext(AuthContext);
+  const { registerInfo, updateRegisterInfo, registerUser, registerError, isRegisterLoading } = useContext(AuthContext);
 
   return (
     <>
-      <Form>
+      <Form onSubmit={registerUser}>
         <Row style={{
           height: "100vh",
           justifyContent: "center",
@@ -40,12 +40,15 @@ const Register = () => {
                 }  
               />
               <Button variant="primary" type="submit">
-                Register
+                {isRegisterLoading ? "Loading..." : "Register"}
               </Button>
 
-              <Alert variant="danger">
-                <p>An error occured</p>
-              </Alert>
+              {
+                registerError?.error && <Alert variant="danger">
+                <p>{registerError?.message}</p>
+                </Alert>
+              }
+
             </Stack>
           </Col>
         </Row>
