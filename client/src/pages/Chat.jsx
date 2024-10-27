187d2d52de40d11926309d2d4a623756285f7a6f
C23069362 <georged8@cardiff.ac.uk>
2024-10-27 14:32:41 -0700

    Implemented functionality for creating new messages with new users.

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -3,6 +3,7 @@ import { Container, Stack } from 'react-bootstrap';
 import { ChatContext } from '../context/ChatContext';
 import { AuthContext } from '../context/AuthContext';
 import UserChat from '../components/chat/UserChat';
+import PotentialChats from '../components/chat/PotentialChats';
 
 const Chat = () => {
 
@@ -14,6 +15,7 @@ const Chat = () => {
 
   return (
     <Container>
+      <PotentialChats />
       {userChats?.length < 1 ? null : <Stack direction="horizontal" gap={4} className='align-items-start'>
         <Stack className='messages-box flex-grow-0 pe-3' gap={3}>
           {isUserChatsLoading && <p>Loading...</p>}
