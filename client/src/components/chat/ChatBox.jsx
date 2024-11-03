4c78480c88c7ca0b45ff29d9a250d96087fd7bd0
C23069362 <georged8@cardiff.ac.uk>
2024-11-02 21:16:09 -0700

    Finished the retreival and displaying of messages onto the chatbox.

--- client/src/components/chat/ChatBox.jsx
+++ client/src/components/chat/ChatBox.jsx
@@ -3,6 +3,7 @@ import { Stack } from 'react-bootstrap';
 import { AuthContext } from '../../context/AuthContext';
 import { ChatContext } from '../../context/ChatContext';
 import { useFetchRecipientUser } from '../../hooks/useFetchRecipient';
+import moment from 'moment';
 
 const ChatBox = () => {
 
@@ -29,6 +30,24 @@ const ChatBox = () => {
       <div className='chat-header'>
         <strong>{recipientUser.name}</strong>
       </div>
+
+      <Stack gap={3} className="messages">
+        {messages &&
+          messages.map((message, index) => (
+          <Stack
+            key={index}
+            className={`${
+              message?.senderId === user?._id
+              ? "message self align-self-end flex-grow-0"
+              : "message align-self-start flex-grow-0"
+            }`}
+          >
+            <span>{message.text}</span>
+            <span className='message-footer'>{moment(message.createdAt).calendar()}</span>
+          
+          </Stack>
+          ))}
+      </Stack>
     </Stack>
   );
 };
